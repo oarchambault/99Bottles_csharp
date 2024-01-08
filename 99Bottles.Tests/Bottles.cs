53139cd862c02ea4142f964b11a6f114a791333b
Olivier Archambault <olivier.archambault@isolutions.ch>
2024-01-08 23:44:36 +0100

    Added converted tests with class skeleton to be tested.

---
+++ 99Bottles.Tests/Bottles.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace _99Bottles.Tests;
+
+public class Bottles
+{
+    public string Verse(int verseNumber)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Verses(int from, int to)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Song()
+    {
+        throw new NotImplementedException();
+    }
+}
